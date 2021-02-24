999e24de72205d0a09ec81287e98773509b6c815
Jonah-Chng <junhaochng@gmail.com>
2021-02-24 12:42:45 +0000

    Adding pwm and motors

--- src/py_driver.py
+++ src/py_driver.py
@@ -1,40 +1,49 @@
 from gpiozero import Motor
 from gpiozero import PWMOutputDevice
 import time
-   
-def test_motors():
-    front_motor = Motor(16, 7, pwm=True) # pwm is GPIO12
-    back_motor = Motor(23, 15, pwm=True) # pwm is GPIO18 # GPIO23, 15 = 1.2, -1.88
-    front_motor_pwm = PWMOutputDevice(12, True, 0, 1000)
-    back_motor_pwm = PWMOutputDevice(18, True, 0, 1000)
-    while(1):
-        back_motor.forward(1.0)
-        back_motor_pwm.value = 1.0
-        front_motor.forward(1.0)
-        front_motor_pwm.value = 1.0
-    #front_motor.forward(1.0)
-    #time.sleep(1)
-    #front_motor.forward(0.5)
-    #time.sleep(1)
-    #front_motor.backward(0.5)
-    #time.sleep(1)
-    #front_motor.backward(1.0)
-    #time.sleep(1) 
 
-    #back_motor.forward(1.0)
-    #time.sleep(1)
-    #back_motor.forward(0.5)
-    #time.sleep(1)
-    #back_motor.backward(0.5)
-    #time.sleep(1)
-    #back_motor.backward(1.0)
-    #time.sleep(1) 
+class Pi_Driver(): 
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        print(self.kwargs)
+        self.left_pwm = PWMOutputDevice(kwargs['left_pin'], kwargs['left_active_high'])
+        self.left_motor = Motor(kwargs['left_forward'], kwargs['left_backward'],kwargs['left_pwm'])
+        self.right_pwm = PWMOutputDevice(kwargs['right_pin'], kwargs['right_active_high'])
+        self.right_motor = Motor(kwargs['right_forward'], kwargs['right_backward'],kwargs['right_pwm'])
+    
+    def test_motors(self):
+        while(1):
+            for i in range(0,6,1):
+                i=(i+5)/10
+                print(i)
+                
+                self.left_pwm.value = i
+                self.left_motor.forward()
+                self.right_pwm.value = i
+                self.right_motor.forward()
+                time.sleep(2.0)
 
-def pin_setup(num_motors, pwm_pins, en_pins):
-    return
-def move(linear_speed, angualr_speed, direction):
-    motor.forward()
+                self.left_pwm.value = i
+                self.left_motor.backward()
+                self.right_pwm.value = i
+                self.right_motor.backward()
+                time.sleep(2.0)
+
+    @property
+    def move(self, linear_speed, angualr_speed, direction): # Use cmd_vel
+        motor.forward()
+        
+        # Check that movement is consistent for both wheels, maybe with a scalar in the PWM.
+    
+    @move.setter
+    def move.setter(self)
 
 if __name__ == '__main__':
-    test_motors()
+    pi_d = Pi_Driver(
+        left_pin= 12,   left_active_high = True,          # left PWM settings 
+        left_forward=16,left_backward=7, left_pwm=True,   # left Motor settings (using L298N)
+        right_pin= 18,   right_active_high = True,          # right PWM settings 
+        right_forward=14,right_backward=23, right_pwm=True,   # right Motor settings (using L298N)    
+    )
 
+    pi_d.test_motors()
